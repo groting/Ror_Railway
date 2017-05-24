9b47bf75a88c6aadca8124028a678c885d322d8b
groting <kyleop@mail.ru>
2017-05-24 20:26:43 +0400

    Delete some files

--- db/migrate/20170510203858_add_order_to_trains.rb
+++
@@ -1,5 +0,0 @@
-class AddOrderToTrains < ActiveRecord::Migration[5.0]
-  def change
-    add_column :trains, :order, :boolean
-  end
-end
--- db/migrate/20170510211145_add_number_to_wagons.rb
+++
@@ -1,5 +0,0 @@
-class AddNumberToWagons < ActiveRecord::Migration[5.0]
-  def change
-    add_column :wagons, :number, :integer
-  end
-end
