00fe34d2465352c307e9127e91d148e1a31cd076
groting <kyleop@mail.ru>
2017-05-24 19:49:54 +0300

    Update railway_station.rb

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -3,9 +3,9 @@ class RailwayStation < ApplicationRecord
   has_many :routes, through: :railway_stations_routes
 
   has_many :trains, foreign_key: :current_station_id
-
-  scope :ordered, -> { joins(:railway_stations_routes).order("railway_stations_routes.position").uniq }
-
+  
+  scope :ordered, -> { select('railway_stations.*, railway_stations_routes.position').joins(:railway_stations_routes).order("railway_stations_routes.position").uniq }
+ 
   def update_position(route, position)
     station_route = station_route(route)
     station_route.update(position: position) if station_route
