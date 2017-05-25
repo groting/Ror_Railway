0cc93db200f4168e7b4ec6cef4eb035abd103ce9
groting <kyleop@mail.ru>
2017-05-25 17:41:01 +0400

    Fix mail adress

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,12 +74,12 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'evening-dusk-37764.herokuapp.com'}  
   config.action_mailer.delivery_method = :smtp  
   config.action_mailer.perform_deliveries = true  
-  config.action_mailer.raise_delivery_errors = false  
+  config.action_mailer.raise_delivery_errors = true  
   config.action_mailer.default :charset => "utf-8"  
   config.action_mailer.smtp_settings = {  
     address: "smtp.gmail.com",
     port: 587,
-    domain: "evening-dusk-37764.herokuapp.com",
+    domain: "heroku.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
